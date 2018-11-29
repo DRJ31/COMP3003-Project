bcd9d3264c65b2038fb8ddd3828053efd441ac88
Junde Yhi <lmy441900@aosc.xyz>
2018-11-29 16:48:03 +0800

    main: add entry for server, not impl

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
+#include <glib.h>
 #include "gui.h"
 
 int main(int argc, char **argv)
 {
-  gui_start(&argc, &argv);
+  if (argc > 1) {
+    if (strcmp("server", argv[1]) == 0) {
+      g_message("server not implemented!");
+    }
+  } else {
+    gui_start(&argc, &argv);
+  }
 
   return 0;
 }
