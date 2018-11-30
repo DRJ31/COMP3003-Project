95cd8c6898129c2d4401a2b801edbcf113880c31
dengrenjie <dengrenjie31@gmail.com>
2018-11-30 22:48:10 +0800

    Update comments

--- src/data.h
+++ src/data.h
@@ -8,14 +8,39 @@ typedef struct {
     char **note;
 } Person;
 
-// *query: The query sentence
-// size[4]: The array to store number of each information
+/**
+ * Get information of a person
+ *
+ * @param query [in] Keywords to get person information
+ * @param size  [in] Array to store size of each category of Person
+ * @return Structure Person of first entity found in database
+ */
 Person *get_person(char *query, int size[4]);
 
+/**
+ * Insert new person into database
+ *
+ * @param person [in] Structure Person of person you want to insert
+ * @return Status code, 1->Success, 0->Failed
+ */
 int insert_info(Person *person);
 
+/**
+ * Check password of user
+ *
+ * @param username
+ * @param password
+ * @return Status code of authentication, 1->Success, 0->Failed
+ */
 int check_pass(char *username, char *password);
 
+/**
+ * Update information for person
+ *
+ * @param person [in] All the information of person to update
+ * @param name   [in] One of person's name to locate him/her
+ * @return Status code of update, 1->Success, 0->Failed
+ */
 int update_info(Person *person, char *name);
 
 #endif
--- src/json.h
+++ src/json.h
@@ -3,9 +3,21 @@
 
 #include "data.h"
 #include "parson.h"
-
+/**
+ * Convert Person struct into json string
+ *
+ * @param root_value [in] Empty json object to store person information
+ * @param person     [in] Person information in
+ * @return Json string of person information
+ */
 char* person_to_json_string (JSON_Value *root_value, Person *person);
 
+/**
+ * Convert json string into person struct
+ *
+ * @param json_string [in] Json pattern of person information
+ * @return A structure Person which contain all the information in json
+ */
 Person* json_string_to_person (char* json_string);
 
 #endif //JSON_JSON_H
