3a05483f87c09278fb3d52b628de4ed5711d113c
Junde Yhi <lmy441900@aosc.xyz>
2018-12-01 11:38:25 +0800

    client: partial impl client_query

--- src/client.c
+++ src/client.c
@@ -1,10 +1,66 @@
-#include "glib.h"
+#include <glib.h>
+#include "http.h"
 #include "client.h"
+
+// Default server address and port. To adjust these settings, use environment
+// variable PB_SERVER and PB_PORT instead.
+static const char *server = "127.0.0.1";
+static const char *port   = "8000";
 
 Person *client_query(const char *q)
 {
   if (!q)
     return NULL;
+
+  // URL encode the query string first
+  char *escaped_q = g_uri_escape_string(q, NULL, TRUE);
+
+  // Path to query
+  size_t path_size = strlen("/") + strlen(escaped_q) + 1; // + 1 NUL
+  char *path = g_malloc(path_size);
+  g_snprintf(path, path_size, "/%s", escaped_q);
+
+  // Check environment variable for updated server and port
+  const char *env_server = g_getenv("PB_SERVER");
+  const char *env_port   = g_getenv("PB_PORT");
+
+  if (env_server)
+    server = env_server;
+  if (env_port)
+    port = env_port;
+
+  // Construct the "Host:" HTTP header content (combine server and port)
+  // I should have decided to implememnt HTTP/1.0...
+  size_t host_size = strlen(server) + strlen(":") + strlen(port) + 1; // + 1 NUL
+  char *server_host = g_malloc(host_size);
+  g_snprintf(server_host, host_size, "%s:%s", server, port);
+
+  char *http = http_request_wrap_message(
+    HTTP_GET,
+    path,
+    (struct HttpHeaders) {
+      .host = server_host,
+      .accept = "application/json",
+    },
+    NULL
+  );
+
+  g_message("HTTP request: ===\n%s\n===", http);
+
+  // Send it over socket
+  // TODO
+  Person *person = NULL;
+
+  g_message("%s: stub, not implemented!", __func__);
+
+  // Free unused memory
+  free(server_host);
+  free(http);
+  free(path);
+  free(escaped_q);
+
+  return person;
+}
 
   g_message("%s: stub, not implemented!", __func__);
 
