d83b40888581d2def7a89b6caf0276a89789c554
Junde Yhi <lmy441900@aosc.xyz>
2018-12-01 11:54:14 +0800

    client: partial impl client_add and client_edit

--- src/client.c
+++ src/client.c
@@ -1,5 +1,6 @@
 #include <glib.h>
 #include "http.h"
+#include "json.h"
 #include "client.h"
 
 // Default server address and port. To adjust these settings, use environment
@@ -67,17 +68,107 @@ bool client_add(Person *person)
   if (!person)
     return false;
 
+  // Serialize person
+  JSON_Value *root_value = json_value_init_object();
+  char *json = person_to_json_string(root_value, person);
+  json_value_free(root_value);
+
+  // Check environment variable for updated server and port
+  const char *env_server = g_getenv("PB_SERVER");
+  const char *env_port   = g_getenv("PB_PORT");
+
+  if (env_server)
+    server = env_server;
+  if (env_port)
+    port = env_port;
+
+  // Construct the "Host:" HTTP header content (combine server and port)
+  size_t host_size = strlen(server) + strlen(":") + strlen(port) + 1; // + 1 NUL
+  char *server_host = g_malloc(host_size);
+  g_snprintf(server_host, host_size, "%s:%s", server, port);
+
+  char *http = http_request_wrap_message(
+    HTTP_POST,
+    "/add",
+    (struct HttpHeaders) {
+      .host = server_host,
+      .content_type = "application/json"
+    },
+    json
+  );
+
+  g_message("HTTP request: ===\n%s\n===", http);
+
+  // Send it over socket
+  // TODO
+  bool result = false;
+
   g_message("%s: stub, not implemented!", __func__);
 
-  return false;
+  // Free unused memory
+  free(http);
+  free(server_host);
+  free(json);
+
+  return result;
 }
 
-bool client_edit(Person *person)
+bool client_edit(const char *name, Person *person)
 {
   if (!person)
     return false;
 
+  // URL encode the resource path to edit
+  char *escaped_name = g_uri_escape_string(name, NULL, TRUE);
+
+  // Path to query
+  size_t path_size = strlen("/") + strlen(escaped_name) + 1; // + 1 NUL
+  char *path = g_malloc(path_size);
+  g_snprintf(path, path_size, "/%s", escaped_name);
+
+  // Serialize person
+  JSON_Value *root_value = json_value_init_object();
+  char *json = person_to_json_string(root_value, person);
+  json_value_free(root_value);
+
+  // Check environment variable for updated server and port
+  const char *env_server = g_getenv("PB_SERVER");
+  const char *env_port   = g_getenv("PB_PORT");
+
+  if (env_server)
+    server = env_server;
+  if (env_port)
+    port = env_port;
+
+  // Construct the "Host:" HTTP header content (combine server and port)
+  size_t host_size = strlen(server) + strlen(":") + strlen(port) + 1; // + 1 NUL
+  char *server_host = g_malloc(host_size);
+  g_snprintf(server_host, host_size, "%s:%s", server, port);
+
+  char *http = http_request_wrap_message(
+    HTTP_POST,
+    path,
+    (struct HttpHeaders) {
+      .host = server_host,
+      .content_type = "application/json"
+    },
+    json
+  );
+
+  g_message("HTTP request: ===\n%s\n===", http);
+
+  // Send it over socket
+  // TODO
+  bool result = false;
+
   g_message("%s: stub, not implemented!", __func__);
 
-  return false;
+  // Free unused memory
+  free(http);
+  free(server_host);
+  free(json);
+  free(path);
+  free(escaped_name);
+
+  return result;
 }
